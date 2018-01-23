bf0e9b5a301eaf1accecf48c6e99323b01fc7e8e
HuboryChen <2574164099@qq.com>
2018-01-23 10:51:56 +0800

    删除“test.txt”文件

---
+++ XTLHPM/src/main/java/net/wicp/satcom/xtlhpm/XtlhpmApplication.java
@@ -0,0 +1,12 @@
+package net.wicp.satcom.xtlhpm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XtlhpmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XtlhpmApplication.class, args);
+	}
+}
